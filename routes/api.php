b8cbc14ed5d8e5e81b016685c4290697e4268d9b
IMAM SODIK <imamsodik00@gmail.com>
2026-05-08 01:24:09 +0700

    adf

--- routes/api.php
+++ routes/api.php
@@ -16,5 +16,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [ApiAuthController::class, 'logout']);
 
     Route::get('/menus', [ApiMenuController::class, 'index']);
-    Route::get('/menus/{id}', [ApiMenuController::class, 'show']);
+    Route::get('/menus/search', [ApiMenuController::class, 'searchMenus']);
 });
